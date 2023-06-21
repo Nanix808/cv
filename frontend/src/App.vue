65c4f8b7dd4f4020524b0ce1df6df8d6942683bd
SavosinVitali <savosinvitali@gmail.com>
2023-06-21 17:03:10 +0300

    frontend start

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script >
+import { ref } from 'vue';
+import AreaUploader from '@/components/Area.vue';
+
+export default {
+  components: {
+    AreaUploader,
+  },
+
+  setup() {
+    const files = ref([]);
+
+    return { files };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <AreaUploader v-model="files">
+    </AreaUploader>
+    {{files }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Area.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="file-uploader">
+    <div
+      class="file-uploader__wrapper"
+    
+    >
+      <input
+        accept="application/pdf"
+        ref="input"
+        class="file-uploader__input"
+        type="file"
+        multiple
+        title=""
+        @change="uploadFile"
+        @dragenter="isDragStarted = true"
+        @dragleave="isDragStarted = false"
+
+      >
+      
+      {{ isDragStarted ? '' : uploadText }}
+      <!-- <img
+        v-show="isDragStarted"
+        src="@/assets/images/upload.svg"
+        class="file-uploader__icon"
+        alt="Загрузите фото"
+      > -->
+    </div>
+    <div class="file-uploader__files">
+      <div
+      
+        class="file-uploader__file"
+      >
+        <button
+          class="file-uploader__remove"
+        
+        />
+        <img
+         
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  computed,
+  ref,
+  // PropType,
+  toRefs,
+} from 'vue';
+
+export default {
+  props: {
+    modelValue: {
+      type: Array,
+      required: false,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  setup(props, { emit }) {
+    const { modelValue } = toRefs(props);
+
+
+    const input = ref(null);
+    const isDragStarted = ref(false);
+
+    const uploadFile = ({ currentTarget }) => {
+      if (currentTarget.files) {
+        console.log([...Array.from(currentTarget.files)])
+        emit('update:modelValue', [...modelValue.value, ...Array.from(currentTarget.files)]);
+      }
+
+      if (input.value) {
+        input.value.value = '';
+      }
+
+      isDragStarted.value = false;
+    };
+
+    // const removefile = (index: number) => {
+    //   emit('update:modelValue', modelValue.value.filter((p, i) => i !== index));
+    // };
+
+    // const getSrc = (file: File) => URL.createObjectURL(file);
+
+    const needToUpload = computed(() => modelValue.value.length);
+
+    const uploadText = computed(() => {
+      if (needToUpload.value > 0) {
+        return `Вы загрузили ${needToUpload.value} 'резюме'`;
+      }
+
+      if (needToUpload.value === 0) {
+        return 'Загрузите резюме';
+      }
+    
+    });
+
+    return {
+      isDragStarted,
+      uploadText,
+      input,
+      uploadFile,
+      // removefile,
+      // needToUpload,
+      // getSrc,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.file-uploader {
+  &__wrapper {
+    position: relative;
+    text-align: center;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 4px dotted #eee;
+    border-radius: 10px;
+    color: rgba(#000, 0.5);
+
+    &--drag {
+      color: rgba(#fff, 0);
+      border-color: #777;
+    }
+  }
+
+  &__input {
+    cursor: pointer;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    opacity: 0;
+  }
+
+  &__files {
+    display: flex;
+    margin-top: 20px;
+  }
+
+  &__file {
+    position: relative;
+    width: 200px;
+    margin-right: 60px;
+
+    img {
+      border-radius: 10px;
+    }
+  }
+
+  &__remove {
+    cursor: pointer;
+    position: absolute;
+    outline: none;
+    top: 0;
+    left: calc(100% + 10px);
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: rgba(#000, 0.05);
+    border: 0;
+
+    &::before {
+      content: "";
+      position: absolute;
+      z-index: 1;
+      top: calc(50% - 1px);
+      left: calc(50% - 5px);
+      width: 10px;
+      height: 2px;
+      background-color: #000;
+    }
+  }
+
+  &__icon {
+    opacity: 0.3;
+    width: 50px;
+  }
+}
+</style>
