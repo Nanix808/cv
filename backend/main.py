2a65a7ac8ab3f8b69419db4903c6f75c4d1cf1e8
SavosinVitali <savosinvitali@gmail.com>
2023-08-12 13:33:15 +0300

    add docker-compose

--- backend/main.py
+++ backend/main.py
@@ -14,8 +14,10 @@ app.include_router(api_router)
 origins = [
     "http://localhost:5173",
      "http://127.0.0.1:5173",
-     "http://127.0.0.1:8080",
-     "http://127.0.0.1:8080",
+     "http://127.0.0.1:8081",
+     "http://localhost:8081",
+     "http://127.0.0.1:8000",
+     "http://localhost:8000",
      "https://scanercv.3s.by",
 ]
 
