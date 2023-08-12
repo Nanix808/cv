a651f11564ab6f9a5ee8bd4f0e7b0e209b5520a0
SavosinVitali <savosinvitali@gmail.com>
2023-08-12 12:59:19 +0300

    add dockerfile frontend update requirements and info

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,8 @@ app.include_router(api_router)
 origins = [
     "http://localhost:5173",
      "http://127.0.0.1:5173",
+     "http://127.0.0.1:8080",
+     "http://127.0.0.1:8080",
      "https://scanercv.3s.by",
 ]
 
